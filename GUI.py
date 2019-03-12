f81b4a0d750e4f3fd17003b52221b0b3b10c40c8
Daniel Weber <danielkweber1@gmail.com>
2019-03-12 22:15:07 +0200

    Initial Commit with Buttons

---
+++ GUI.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+window_title = root.title('Chess Engine')
+title_frame = Frame(root)
+button_frame = Frame(root)
+welcome_text = Label(title_frame, text = 'Chess Engine')
+easy_AI_button = Button(button_frame, text = 'Start Easy AI Game', bg = 'blue', fg = 'white')
+medium_AI_button = Button(button_frame, text = 'Start Medium AI Game', bg = 'blue', fg = 'white')
+hard_AI_button = Button(button_frame, text = 'Start Hard AI Game', bg = 'blue', fg = 'white')
+title_frame.pack(side = TOP)
+button_frame.pack(side = BOTTOM)
+welcome_text.pack()
+easy_AI_button.pack()
+medium_AI_button.pack()
+hard_AI_button.pack()
+
+root.mainloop()
